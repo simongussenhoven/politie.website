29001c26379a7386d72e9fc5dd446c052d2a1ead
Simon Gussenhoven <simongussenhoven@gmail.com>
2024-01-13 16:10:30 +0100

    Reinstate flowbite

--- app.vue
+++ app.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="app">
+        <nav-bar />
         <NuxtLayout>
             <NuxtPage />
         </NuxtLayout>
     </div>
 </template>
 <script lang="ts" setup>
-
+import NavBar from '@/components/NavBar.vue'
 </script>
